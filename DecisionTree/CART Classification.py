ecc1ac008dd64ee687f1c62b9022ea5f26b10b80
Mohamed Noordeen <mnoordeen@671207523NB001.local>
2018-01-09 19:46:46 +0530

    Decision tree example

---
+++ DecisionTree/CART Classification.py
@@ -0,0 +1,17 @@
+# CART Classification
+import pandas
+from sklearn import cross_validation
+from sklearn.tree import DecisionTreeClassifier
+url = "https://goo.gl/vhm1eU"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+num_folds = 10
+num_instances = len(X)
+seed = 7
+kfold = cross_validation.KFold(n=num_instances, n_folds=num_folds, random_state=seed)
+model = DecisionTreeClassifier()
+results = cross_validation.cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
